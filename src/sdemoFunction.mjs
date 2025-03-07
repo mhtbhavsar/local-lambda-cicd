8a672c9c09d1574acef919dad5d79fefd781f3c4
mhtbhavsar <mhtbhavsar@gmail.com>
2025-03-07 13:38:23 +0530

    added 2nd lambda

--- src/sdemoFunction.mjs
+++ src/sdemoFunction.mjs
@@ -3,7 +3,7 @@ export const handler = async (event) => {
     console.log("hello");
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from Github deployed 2nd Lambda 001!'),
+      body: JSON.stringify('Hello from Github deployed Lambda 001!'),
     };
     return response;
   };
