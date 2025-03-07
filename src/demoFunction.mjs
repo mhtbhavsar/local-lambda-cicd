ab1cb80b1520e7b547623654474af89bc58ce840
mohit bhavsar <mhtbhavsar@gmail.com>
2025-03-07 13:08:55 +0530

    Update demoFunction.mjs

--- src/demoFunction.mjs
+++ src/demoFunction.mjs
@@ -3,7 +3,7 @@ export const handler = async (event) => {
     console.log("hello");
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from Github deployed Lambda 1!'),
+      body: JSON.stringify('Hello from Github deployed Lambda 001!'),
     };
     return response;
   };
